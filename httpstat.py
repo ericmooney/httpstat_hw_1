a95baed9a12cf8470d12559b7b94749ad5b58353
Eric Mooney <eric.c.mooney@gmail.com>
2016-09-16 14:44:52 -0400

    truncate strings longer than 100 characters

--- httpstat.py
+++ httpstat.py
@@ -40,7 +40,7 @@ curl_format = """{
 }"""
 
 https_template = """
-[
+
     |
     |   DNS lookup
     |   {a0000}
@@ -78,9 +78,8 @@ total:{b0004}
 
 
 
-http_template = """
- [
-    
+http_template = """ 
+
     |   DNS lookup
     |   {a0000}
     |           
@@ -103,7 +102,6 @@ starttransfer:{b0003}
     |
 
 total:{b0004} 
-
 
 """[1:]
 
@@ -252,7 +250,12 @@ def main():
             table_rows.append([green(p1), cyan(p2)])
         else:
             pos = line.find(':')
-            table_rows.append([grayscale[14](line[:pos]), red(line[pos + 2:])])
+            if len(line[pos + 2:]) > 100:
+                detail = line[pos + 2:102]
+            else:
+                detail = line[pos + 2:]
+
+            table_rows.append([grayscale[14](line[:pos]), red(detail)])
 
     print(tabulate(table_rows, table_headers, tablefmt="rst"))
 
